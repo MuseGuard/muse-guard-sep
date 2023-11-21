db1744eb8ce2a239d086d9429347f25028696091
Vatsal Jariwala <105443900+vatsal8784@users.noreply.github.com>
2023-11-21 18:23:10 +0100

    fixed sensor data and graph

--- src/Hooks/useSensorData.js
+++ src/Hooks/useSensorData.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { Line } from "react-chartjs-2";
 import "chartjs-adapter-date-fns";
 import { Chart, registerables } from 'chart.js';
+
+
 Chart.register(...registerables)
 
 const useSensorData = () => {
@@ -17,9 +19,9 @@ const useSensorData = () => {
       .get(url)
       .then((response) => {
         console.log(response);
-        //setSensorData(response.data);
+        setSensorData(response.data);
         
-        //setIsLoading(false);
+        setIsLoading(false);
       })
       .catch((error) => {
         setIsLoading(false);
@@ -29,7 +31,7 @@ const useSensorData = () => {
 
   const fetchLastNValue = (n) => {
     setIsLoading(true);
-    axios.get(`${url}?limit=${n}`)
+    axios.get(url)
     .then((response) => {
       setSensorData(response.data);
       setIsLoading(false);
@@ -41,28 +43,39 @@ const useSensorData = () => {
   };
 
   useEffect(() => {
+    // Fetch data when the component is mounted
     fetchData();
-  }, []); // Fetch data when the component is mounted
 
-  // const chartData = {
-  //   labels: sensorData ? sensorData.map((data) => data.timestamp) : [],
-  //   datasets: [
-  //     {
-  //       label: "Sensor Data",
-  //       data: sensorData ? sensorData.map((data) => data.value) : [],
-  //       fill: false,
-  //       borderColor: "rgb(75, 192, 192)",
-  //       tension: 0.1,
-  //     },
-  //   ],
-  // };
+    // Set up an interval to fetch data every 5 seconds
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 5000);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []); 
+// Fetch data when the component is mounted
+
+  const chartData = {
+    labels: sensorData ? sensorData.map((data) => data.time) : [],
+    datasets: [
+      {
+        label: "Sensor Data",
+        data: sensorData ? sensorData.map((data) => data.temperature) : [],
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
 
   return {
     sensorData,
     isLoading,
     fetchData,
     fetchLastNValue,
-    // LineChart:<Line data={chartData}/>
+    chartData,
+    LineChart:<Line data={chartData}/>
   };
 };
 
--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -1,79 +1,7 @@
-// import React from "react";
-// import useSensorData from "../Hooks/useSensorData";
-// import { Line } from "react-chartjs-2";
-// import { useState } from "react";
-// import { useEffect } from "react";
-// import '../Styles/Info.css';
-// import FakeTest from "../Hooks/FakeTest";
-
-// const Info = () => {
-//     const { sensorData, isLoading, fetchData, LineChart, fetchLastNValue } = FakeTest();
-
-//     const [showLastNValues, setShowLastNValues] = useState(false);
-
-//     const handleRefreshClick = () => {
-//         fetchData();
-//         setShowLastNValues(false);
-//     };
-
-//     const handleLastNValuesClick = () => {
-//         fetchLastNValue(10);
-//         setShowLastNValues(true);
-//     }
-
-//     useEffect(() => {
-//         // Cleanup the previous chart instance when the component unmounts
-//         return () => {
-//             if (LineChart && LineChart.chartInstance) {
-//                 LineChart.chartInstance.destroy();
-//             }
-//         };
-//     }, [LineChart]);
-
-//     return (
-//         <div className="info-container">
-//             <h2>Sensor Data</h2>
-//             {isLoading ? (
-//                 <p>Loading sensor data...</p>
-//             ) : (
-//                 <div>
-//                     {sensorData ? (
-//                         <div>
-//                             <p>Sensor Value: {sensorData}</p>
-//                             {/* Replace 'age' with the actual property name you have in your data */}
-//                         </div>
-//                     ) : (
-//                         <p>No data available.</p>
-//                     )}
-
-//                     <button onClick={handleRefreshClick}>Refresh</button>
-//                     <button onClick={handleLastNValuesClick}>Show Last 10 Values</button>
-
-//                     {showLastNValues && (
-//                         <div className="chart-container">
-//                             {/* Adjust chart styling based on your requirements */}
-//                             <Line
-//                                 data={LineChart.props.data}
-//                                 options={{
-//                                     responsive: true,
-//                                     maintainAspectRatio: false,
-//                                 }}
-//                             />
-//                         </div>
-//                     )}
-//                 </div>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default Info;
-
 import React, { useState } from "react";
 import useSensorData from "../Hooks/useSensorData";
 import { Line } from "react-chartjs-2";
 import "../Styles/Info.css";
-import FakeTest from "../Hooks/FakeTest";
 
 const Info = () => {
   const {
@@ -82,6 +10,7 @@ const Info = () => {
     fetchData,
     fetchLastNValue,
     chartData,
+    //LineChart,
   } = useSensorData();
 
   const [showLastNValues, setShowLastNValues] = useState(false);
@@ -103,10 +32,9 @@ const Info = () => {
         <p>Loading sensor data...</p>
       ) : (
         <div>
-          {sensorData ? (
+          {sensorData && sensorData.length > 0 ? (
             <div>
-              <p>Sensor Value: {sensorData[0].value}</p>
-              {/* Replace 'value' with the actual property name you have in your data */}
+              <p>Sensor Value: {sensorData[0].temperature}</p>
             </div>
           ) : (
             <p>No data available.</p>
@@ -115,20 +43,42 @@ const Info = () => {
           <button onClick={handleRefreshClick}>Refresh</button>
           <button onClick={handleLastNValuesClick}>Show Last 10 Values</button>
 
-          {showLastNValues && (
+          {showLastNValues && sensorData && sensorData.length > 0 && (
             <div className="chart-container">
-              <Line
-                data={chartData}
-                options={{
-                  responsive: true,
-                  maintainAspectRatio: false,
-                  scales: {
-                    x: {
-                      type: "linear", // Use 'linear' for numeric data on X-axis
+              {/* Separate div for Sensor Value */}
+              <div className="sensor-value-container">
+                <p>Sensor Value: {sensorData[0].temperature}</p>
+              </div>
+
+              {/* Separate div for the Line Chart */}
+              <div className="line-chart-container">
+                <Line
+                  data={chartData}
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    scales: {
+                      x: {
+                        type: 'time',
+                        time: {
+                          unit: 'minute',
+                          tooltipFormat: 'YYYY-MM-DD HH:mm:ss',
+                        },
+                        title: {
+                          display: true,
+                          text: 'Timestamp',
+                        },
+                      },
+                      y: {
+                        title: {
+                          display: true,
+                          text: 'Temperature',
+                        },
+                      },
                     },
-                  },
-                }}
-              />
+                  }}
+                />
+              </div>
             </div>
           )}
         </div>
