048572492e03b8ee6abadb570f7818adba34b6ae
Amelia Guesmi <100846210+Amelia1247@users.noreply.github.com>
2023-11-24 20:42:14 +0100

    fixed auth page

--- src/Hooks/Auth.js
+++ src/Hooks/Auth.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { useState } from "react";
 
 const Auth = () => {
-    const [password, setPassword] = useState("");
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [error, setError] = useState(null);
+  const [password, setPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [error, setError] = useState(null);
 
-    const authenticate = async () => {
+  const authenticate = async () => {
         try {
             // Replace 'YOUR_API_ENDPOINT' with the actual endpoint to check the password
             const response = await axios.post('YOUR_API_ENDPOINT', { password });
@@ -22,36 +22,36 @@ const Auth = () => {
         }
     };
 
-    const changePassword = async (newPassword) => {
-        try {
-            // Replace 'YOUR_API_ENDPOINT' with the actual endpoint to change the password
-            const response = await axios.post('YOUR_API_ENDPOINT/change-password', { newPassword });
+  const changePassword = async (newPassword) => {
+    try {
+// Replace 'YOUR_API_ENDPOINT' with the actual endpoint to change the password
+      const response = await axios.post('YOUR_API_ENDPOINT/change-password', { newPassword });
 
-            if (response.data.success) {
-                setPassword(newPassword);
-                clearError();
-            } else {
-                setError("Failed to change password");
-            }
-        } catch (error) {
-            console.error("Error during password change:", error);
-            setError("Error during password change. Please try again.");
-        }
-    };
+      if (response.data.success) {
+        setPassword(newPassword);
+        clearError();
+      } else {
+        setError("Failed to change password");
+      }
+    } catch (error) {
+      console.error("Error during password change:", error);
+      setError("Error during password change. Please try again.");
+    }
+  };
 
-    const clearError = () => {
-        setError(null);
-    };
+  const clearError = () => {
+    setError(null);
+  };
 
-    return {
-        password,
-        setPassword,
-        isAuthenticated,
-        authenticate,
-        changePassword,
-        error,
-        clearError,
-    };
+  return {
+    password,
+    setPassword,
+    isAuthenticated,
+    authenticate,
+    changePassword,
+    error,
+    clearError,
+  };
 };
 
 export default Auth;
