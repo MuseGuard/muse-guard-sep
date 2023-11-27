d7cb8321b06ab3a1a53283079c2a3bcd6cd3f665
Vatsal Jariwala <105443900+vatsal8784@users.noreply.github.com>
2023-11-27 22:18:33 +0100

    Jwt authentication added

--- src/Hooks/Auth.js
+++ src/Hooks/Auth.js
@@ -1,57 +1,44 @@
-import axios from "axios";
-import { useState } from "react";
+import  { useState } from 'react';
+import axios from 'axios';
 
 const Auth = () => {
-    const [password, setPassword] = useState("");
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [error, setError] = useState(null);
+  const [pin, setPin] = useState('');
+  const [token, setToken] = useState('');
+  const [protectedMessage, setProtectedMessage] = useState('');
 
-    const authenticate = async () => {
-        try {
-            // Replace 'YOUR_API_ENDPOINT' with the actual endpoint to check the password
-            const response = await axios.post('YOUR_API_ENDPOINT', { password });
+  
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(' ', { pin });
+      setToken(response.data.token);
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
 
-            if (response.data.authenticated) {
-                setIsAuthenticated(true);
-            } else {
-                setError("Incorrect password");
-            }
-        } catch (error) {
-            console.error("Error during authentication:", error);
-            setError("Error during authentication. Please try again.");
-        }
-    };
+  
+  const accessProtectedEndpoint = async () => {
+    try {
+      const response = await axios.get(' ', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setProtectedMessage(response.data.message);
+    } catch (error) {
+      console.error('Error accessing protected endpoint:', error);
+    }
+  };
 
-    const changePassword = async (newPassword) => {
-        try {
-            // Replace 'YOUR_API_ENDPOINT' with the actual endpoint to change the password
-            const response = await axios.post('YOUR_API_ENDPOINT/change-password', { newPassword });
-
-            if (response.data.success) {
-                setPassword(newPassword);
-                clearError();
-            } else {
-                setError("Failed to change password");
-            }
-        } catch (error) {
-            console.error("Error during password change:", error);
-            setError("Error during password change. Please try again.");
-        }
-    };
-
-    const clearError = () => {
-        setError(null);
-    };
-
-    return {
-        password,
-        setPassword,
-        isAuthenticated,
-        authenticate,
-        changePassword,
-        error,
-        clearError,
-    };
+  return {
+    pin,
+    setPin,
+    token,
+    setToken,
+    protectedMessage,
+    handleLogin,
+    accessProtectedEndpoint,
+  };
 };
 
 export default Auth;
