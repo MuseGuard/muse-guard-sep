37005b7878be523ea2ffc451d047830003493c2a
Amelia Guesmi <100846210+Amelia1247@users.noreply.github.com>
2023-12-02 16:33:01 +0100

    template for the notificationCards

--- src/Components/NotficationCard.jsx
+++ src/Components/NotficationCard.jsx
@@ -1,18 +1,30 @@
+import LightNotification  from "../Assets/Light.png"
+import  tempNotification  from "../Assets/Temp.png"
+import  humidityNotification  from "../Assets/Humidity.png"
 const NotificationCard = () => {
     return ( 
-        <div className="bg-red-300 flex flex-col w-[230px] h-[100px] items-center ">
-                <h1 className="text-lg"> title </h1>
-                <div className="bg-green-300  flex flex-col h-[100px] w-[200px]"> 
-                    <div className="bg-yellow-500 h-1/3 w-[200px]">
-                        <h2> bro</h2>
-                        
+        <div className="bg-white/40 shadow-xl flex flex-col w-[230px] h-[130px] items-center rounded-3xl">
+                <h1 className="text-lg"> Mona Lisa </h1>
+                <h2 className="text-sm "> 02/12/2023 16:17 </h2>
+                <div className="flex flex-col h-[100px] w-[210px]"> 
+                    <div className=" h-1/3 w-[210px] flex flex-row justify-between items-center px-2">
+                       
+                        <h2 className="text-sm"> Light levels too dim!</h2>
+                        <img src={LightNotification} alt="LightNotification" />
                      </div>
                     
-                    <div className="bg-purple-500 h-1/3 w-[200px]"> 
-                    <h2>Mannnn</h2> </div>
-                    <div className="bg-white-300 h-1/3 w-[200px]">
-                        <h3> yes</h3> </div>
+                    <div className="h-1/3 w-[210px] flex flex-row  items-center justify-between px-2"> 
+                    
+                    <h2 className="text-sm">Temperature too high!</h2> 
+                    <img  src={tempNotification} alt="tempNotification" />
 
+                    </div>
+
+                    <div className="h-1/3 w-[210px] flex flex-row items-center justify-between px-2">
+                    
+                        <h2 className="text-sm" > Too humid!</h2> 
+                         <img src={humidityNotification} alt="humidityNotification" />
+                         </div>
                 </div>
             
 
--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -1,7 +1,7 @@
 import NotificationCard from "./NotficationCard";
 const Notifications = () => {
     return ( 
-        <div className="flex flex-col items-center px-4 py-4 bg-black/40 w-1/4">
+        <div className="flex flex-col items-center px-4 py-4 bg-black/40 w-1/4 space-y-5">
                 <h1 className="text-5xl"> Notifications </h1>
                 <NotificationCard />
             </div>
