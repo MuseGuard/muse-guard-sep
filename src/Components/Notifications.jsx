4504a14d419a060b31a2b87f69075e44c2c9f58e
Amelia Guesmi <100846210+Amelia1247@users.noreply.github.com>
2023-12-06 14:34:02 +0100

    fix warningIcon animaton
    it was displaying two different reactions in Edge and Chrome browsers

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -1,18 +1,27 @@
 import NotificationCard from "./NotficationCard";
 import warningIcon from "../Assets/warning.png";
+import '../Styles/Animation.css';
+import React, { useState } from 'react';
 
 const Notifications = () => {
+  const [isShaking, setShaking] = useState(true);
+
+  const handleShakeClick = () => {
+    setShaking(!isShaking);
+  };
+
   return (
     <div className="flex flex-col items-center px-4 py-4 bg-black/40 w-full md:w-1/2 lg:w-1/3 xl:w-1/4 space-y-5">
-       
-
-         <h1 className="text-5xl">Notifications</h1>
- <img src={warningIcon} alt="warningIcon" 
-           className="w-10 h-10 transition-transform transform hover:scale-110 animate-shake " />
-       <NotificationCard />
+      <h1 className="text-5xl">Notifications</h1>
+      <img
+        src={warningIcon}
+        alt="warningIcon"
+        className={`w-10 h-10 transition-transform transform hover:scale-110 ${isShaking ? 'animate-WarningIcon' : ''}`}
+        onClick={handleShakeClick}
+      />
+      <NotificationCard />
     </div>
   );
 };
 
 export default Notifications;
-
