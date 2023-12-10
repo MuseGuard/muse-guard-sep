8c9d729d45b5a805775f4a7e20baf7bc6574c438
Vatsal Jariwala <105443900+vatsal8784@users.noreply.github.com>
2023-12-10 19:26:52 +0100

    cd check

--- src/Test/Artefact.test.js
+++ src/Test/Artefact.test.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import { render, fireEvent, waitFor , screen, within} from '@testing-library/react';
 import axios from 'axios';
+import { fetchArtefactData } from '../Hooks/ManagingArtefact';
 import Artefact from '';
 
 jest.mock('axios');
 
 describe('Artefact component', () => {
   it('should fetch artefact data and update state', async () => {
-    const responseData = [{ id: 1, name: 'Artefact 1' }];
-    axios.get.mockResolvedValueOnce({ data: responseData });
-
-    const { getByText, getByTestId } = render(<Artefact />);
-
-    expect(screen.getByText(/Loading/i)).toBeInTheDocument();
-
-    // Wait for useEffect to fetch data
-    try {
-      // Wait for useEffect to fetch data
-      await waitFor(() => within(screen.findByText(/Artefact 1/i)).toBeInTheDocument());
-    } catch (error) {
-      // Handle the error, or rethrow it if needed
-      console.error('Error in the waitFor block:', error);
-      throw error;
-    }
-  });
-
-  it('should handle input changes', () => {
-    const { getByTestId } = render(<Artefact />);
-
-    fireEvent.change(screen.getByTestId('name-input'), {
-      target: { name: 'name', value: 'New Artefact' },
-    });
-
-    expect(screen.getByTestId('name-input').value).toBe('New Artefact');
+   const artefact = await fetchArtefactData();
+   expect(artefact).toEqual(13);
   });
 
   // Add more tests for other functions such as handleArtefact, handleDeleteArtefact, etc.
